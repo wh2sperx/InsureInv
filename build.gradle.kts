b2cc78ecc245bfeb55aa95f396bd9db605ad25a1
wh2sperx <wh2sperx@gmail.com>
2026-03-21 16:59:30 +0700

    fix: add missing adventure-key

--- build.gradle.kts
+++ build.gradle.kts
@@ -85,6 +85,7 @@ dependencies {
     implementation("net.kyori:adventure-platform-bukkit:4.4.1")
     implementation("net.kyori:adventure-text-minimessage:4.26.1")
     implementation("net.kyori:examination-api:1.3.0")
+    implementation("net.kyori:adventure-key:4.26.1")
     implementation("com.github.wh2sperx.hyticallib-i18n:hyticallib-i18n-bukkit:main-SNAPSHOT")
     implementation("com.github.wh2sperx.hyticallib-i18n:hyticallib-i18n-core:main-SNAPSHOT")
 }
@@ -119,6 +120,7 @@ tasks.shadowJar {
         include(dependency("net.kyori:adventure-platform-bukkit"))
         include(dependency("net.kyori:adventure-text-minimessage"))
         include(dependency("net.kyori:examination-api"))
+        include(dependency("net.kyori:adventure-key"))
         include(dependency("com.github.wh2sperx.hyticallib-i18n:hyticallib-i18n-bukkit"))
         include(dependency("com.github.wh2sperx.hyticallib-i18n:hyticallib-i18n-core"))
     }
